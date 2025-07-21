8ba39b155d60cb5b5bc9fddaefc08f8015d6cf44
celsoaquino <celso.aquino@ymail.com>
2025-07-20 23:15:13 -0300

    implement UserRepository and RoleRepository

---
+++ src/main/java/dev/celsoaquino/usermanagementapi/repository/RoleRepository.java
@@ -0,0 +1,9 @@
+package dev.celsoaquino.usermanagementapi.repository;
+
+import dev.celsoaquino.usermanagementapi.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+}
