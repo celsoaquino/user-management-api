609af0693311999dd9d9a6560bf57b87a3a38ac2
celsoaquino <celso.aquino@ymail.com>
2025-07-21 00:55:41 -0300

    implement AuthService and JwtService for authentication and JWT generation

---
+++ src/main/java/dev/celsoaquino/usermanagementapi/service/AuthService.java
@@ -0,0 +1,18 @@
+package dev.celsoaquino.usermanagementapi.service;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthService {
+
+    private final JwtService jwtService;
+
+    public AuthService(JwtService jwtService) {
+        this.jwtService = jwtService;
+    }
+
+    public String authenticate(Authentication authentication) {
+        return jwtService.generateToken(authentication);
+    }
+}
